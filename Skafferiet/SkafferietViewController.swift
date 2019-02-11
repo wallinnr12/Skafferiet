123fdc0f9e01fc7b6a5ffb2e697039473cb81f8f
Johan Wallin <wallinnr12@gmail.com>
2019-02-11 20:42:39 +0100

    View controllers

---
+++ Skafferiet/SkafferietViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SkafferietViewController.swift
+//  Skafferiet
+//
+//  Created by Johan on 2019-02-11.
+//  Copyright © 2019 Johan W. All rights reserved.
+//
+
+import UIKit
+
+class SkafferietViewController: UIViewController {
+
+    @IBOutlet weak var itemTextField: UITextField!
+    @IBOutlet weak var itemImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
