4b6227cec08f546bbcd684cb8e59c3502549fe99
Johan Wallin <wallinnr12@gmail.com>
2019-02-11 19:42:06 +0100

    Initial Commit

---
+++ Skafferiet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skafferiet
+//
+//  Created by Johan on 2019-02-11.
+//  Copyright © 2019 Johan W. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
