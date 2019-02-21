d53398bbe2b0e296de2788a7a201dbf13667b562
Johan Wallin <wallinnr12@gmail.com>
2019-02-21 20:54:50 +0100

    We can create items and pull them out of coredata

--- Skafferiet/SkafferietViewController.swift
+++ Skafferiet/SkafferietViewController.swift
@@ -7,22 +7,52 @@
 //
 
 import UIKit
+import CoreData
 
-class SkafferietViewController: UIViewController {
+class SkafferietViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var itemTextField: UITextField!
     @IBOutlet weak var itemImageView: UIImageView!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+       
     }
-
+    
     @IBAction func photosTapped(_ sender: Any) {
+        
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+       
+        itemImageView.image = image
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     
     @IBAction func cameraTapped(_ sender: Any) {
     }
     @IBAction func addTapped(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let item = Item(context: context)
+        item.title = itemTextField.text
+        item.image = UIImage.pngData(itemImageView.image!)()
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
 }
--- Skafferiet/ViewController.swift
+++ Skafferiet/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items : [Item] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            items = try context.fetch(Item.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            
+        }
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.imageView?.image = UIImage(data: item.image as! Data)
+        return cell
+    }
 }
 
